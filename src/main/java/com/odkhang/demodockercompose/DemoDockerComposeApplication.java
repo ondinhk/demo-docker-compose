68412c272e9ace173fcc1de56b8f731bc3fd2080
ODKhang <ondinhk@gmail.com>
2022-08-19 23:43:00 +0700

    create app

---
+++ src/main/java/com/odkhang/demodockercompose/DemoDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.odkhang.demodockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerComposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odkhang/demodockercompose/DemoDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odkhang.demodockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
